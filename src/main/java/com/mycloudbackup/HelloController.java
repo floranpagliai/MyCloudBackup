8eefa55700e52ccad2f72a44979eefaf821d4711
Floran Pagliai <floran.pagliai@gmail.com>
2015-05-28 17:33:04 +0800

    Add hibernate

--- src/main/java/com/mycloudbackup/HelloController.java
+++ src/main/java/com/mycloudbackup/HelloController.java
@@ -5,12 +5,26 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) throws IOException, InterruptedException {
+        model.addAttribute("message", "Hello world!");
+        String dbName = "wineot";
+        String dbUser = "root";
+        String dbPass = "root";
+        String executeCmd = "";
+        executeCmd = "mysqldump -u " + dbUser + " -p" + dbPass + " " + dbName + " -r backup.sql";
+
+        Process runtimeProcess = Runtime.getRuntime().exec(executeCmd);
+        int processComplete = runtimeProcess.waitFor();
+        if (processComplete == 0) {
+            return "Backup taken successfully";
+        } else {
+            return "Could not take mysql backup";
+        }
+    }
 }
